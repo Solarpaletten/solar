73707ddbc78f39479a7e7093d6b35a646be1eef3
Solarpaletten <solarpaletten@gmail.com>
2024-10-10 23:33:42 +0300

    fr

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Добро пожаловать в моё приложение! Solar</h1>
+      <p>Это стартовая страница вашего React-приложения Solar.</p>
+    </div>
+  );
+}
+
+export default App;
+git init
